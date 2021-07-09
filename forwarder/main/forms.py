b373482e37ad09a713220ce8c30a019a212ef795
Evgeniya <evgenia@agamova.ru>
2021-07-09 20:25:40 +0300

    commit

--- forwarder/main/forms.py
+++ forwarder/main/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class ForwarderForm(forms.Form):
-    access_code = forms.CharField(max_length=100, label='Enter access code')
+    access_code = forms.CharField(max_length=20, label='Access code')
